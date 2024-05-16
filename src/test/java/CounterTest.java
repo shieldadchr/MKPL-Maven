0033cd5d105759796c0f317530dbf624416ac644
shieldadchr <127294977+shieldadchr@users.noreply.github.com>
2024-05-16 17:46:40 +0700

    .

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -5,6 +5,15 @@ import src.main.java.*;
 import org.junit.Test;
 
 public class CounterTest {
+	@Test
+		public void testIncrement() {
+			Counter testCounter = new Counter();
+			
+			for(int i = 1; i <10; i++) {
+				testCounter.increment();
+				assertEquals(testCounter.getCount(), i);
+			}
+		}
 
 
 	@Test
