6115b6c46b981c7c73144e34866608b90417932a
shieldadchr <127294977+shieldadchr@users.noreply.github.com>
2024-05-16 18:35:24 +0700

    testing

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -5,15 +5,6 @@ import src.main.java.*;
 import org.junit.Test;
 
 public class CounterTest {
-    @Test
-    public void testIncrement() {
-        Counter testCounter = new Counter();
-        
-        for(int i = 1; i <10; i++) {
-            testCounter.increment();
-            assertEquals(testCounter.getCount(), i);
-        }
-    }
 
     @Test
     public void testIncrementWithInitialValue() { // Renamed the method
